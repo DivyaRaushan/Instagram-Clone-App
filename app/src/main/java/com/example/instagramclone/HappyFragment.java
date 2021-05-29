26d232f141a64519604f1f8fc76ce042b4ecf53a
Divya Raushan <hs68217@gmail.com>
2021-05-29 11:48:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/HappyFragment.java
@@ -0,0 +1,27 @@
+package com.example.instagramclone;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HappyFragment extends Fragment {
+
+    public HappyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_happy, container, false);
+
+        return view;
+    }
+}
